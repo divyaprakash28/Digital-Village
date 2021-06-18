9e7740283fa6dc6994aa410c8ee86e8bc9a356c7
divyaprakash28 <divyprakash.om@gmail.com>
2021-06-18 19:15:24 +0530

    listen port

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.post("/", function(req, res) {
     res.render("praman", { name: name, fname: fname, jaati: "आहीर", upjaati: "आहीर" });
 })
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+
+app.listen(port, function() {
+    console.log("Server has been started succesfully");
 });
