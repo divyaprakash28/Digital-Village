e1d1192af65675644b97b72386f98c06640e4f03
divyaprakash28 <divyprakash.om@gmail.com>
2021-06-19 15:43:56 +0530

    letterpad

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const today = new Date();
+const day = today.getDate(); // 24
+const month = today.getMonth(); // 10 (Month is 0-based, so 10 means 11th Month)
+const year = today.getFullYear();
+
 app.get("/", function(req, res) {
     res.render("index");
+})
+
+app.post("/letterpad", function(req, res) {
+    var post = req.body.post;
+    res.render("letterpad", { post: post, day: day, month: month + 1, year: year });
+})
+
+app.get("/compose", function(req, res) {
+    var post = req.body.postBody;
+    res.render("compose");
+})
+app.get("/main", function(req, res) {
+    var post = req.body.postBody;
+    res.render("main");
 })
 
 app.get("/praman", function(req, res) {
@@ -16,14 +35,13 @@ app.get("/praman", function(req, res) {
     res.render("praman", { name: ".........", fname: "..............", aay: " ", income: "", rupaye: " " });
 });
 
+
+
 app.post("/", function(req, res) {
     var name = req.body.name;
     var fname = req.body.fname;
     var pramantype = req.body.type;
-    const today = new Date();
-    const day = today.getDate(); // 24
-    const month = today.getMonth(); // 10 (Month is 0-based, so 10 means 11th Month)
-    const year = today.getFullYear();
+
     if (pramantype === "आय प्रमाणपत्र") {
         var income = req.body.income;
         res.render("praman", { name: name, fname: fname, jaati: "अहीर ", upjaati: "अहीर ", aay: "तथा इनके माता पिता द्वारा होने वाली कुल वार्षिक आय ", rupaye: "रुपये है", type: pramantype, income: income, day: day, month: month + 1, year: year });
